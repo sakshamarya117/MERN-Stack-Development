0a6a0460e25fb9bb23b87a8f3a99e14221a96c3f
sakshamarya117 <sakshamarya117@mail.com>
2023-07-24 15:50:58 +0100

    bootstrap

---
+++ Section 1/index.js
@@ -0,0 +1,28 @@
+console.log('Hello Javascript!!');
+
+console.log('Another Line');
+
+age=27;
+console.log(age);
+// -------------------------------------------------------------------------node --watch index
+
+console.log(typeof age);
+message='nice';
+console.log(message);
+isHungary=true;
+console.log(typeof isHungary);
+// -----------------------------------------------------------------------Condition
+if(age>=18){
+   console.log('you r eligible for DL');
+   x='this should be global';
+//    let y= 'this should be local';
+   var z='this should be local';
+//    const pi=3.14
+}
+else{
+    console.log('you r not eligible for DLS');
+}
+console.log(x);
+// console.log(y);
+console.log(z);
+// console.log(pi);
